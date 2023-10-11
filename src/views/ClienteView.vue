d5dacd915a6cd70b013b37bfd7f25b69a39a2e3a
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-11 17:03:27 -0300

    feat: tela de cadastro clientes

--- src/views/ClienteView.vue
+++ src/views/ClienteView.vue
@@ -1,9 +1,47 @@
 <template>
-    <div>Clientes</div>
+    <div class="mt-4">
+        <v-card>
+            <v-card-title>
+                Cadastrar Cliente
+            </v-card-title>
+            <v-card-text>
+                <v-form class="px-3" ref="form">
+                    <v-text-field label="Nome" v-model="title" prepend-icon="mdi-alphabetical" :rules="inputRules"></v-text-field>
+                    <v-text-field label="Documento" v-model="content" prepend-icon="mdi-id-card"
+                        :rules="inputRules"></v-text-field>
+                        <v-text-field label="Contato" v-model="title" prepend-icon="mdi-phone" :rules="inputRules"></v-text-field>
+                        <v-text-field label="Email" v-model="title" prepend-icon="mdi-email" :rules="inputRules"></v-text-field>
+
+                    <div class="text-right">
+                        <v-btn class="mx-0 mt-3 mr-2" @click="submit">Cadastrar</v-btn>
+                        <v-btn class="mx-0 mt-3">Consultar</v-btn>
+                    </div>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </div>
 </template>
 
 <script>
-export default{
-    name:"ClienteView"
+export default {
+    name: "ClienteView",
+    data() {
+        return {
+            title: "",
+            content: "",
+            due: "",
+            menu: false,
+            inputRules: [
+                v => v.length >= 3 || 'Minimo de 3 caracteres'
+            ]
+        }
+    },
+    methods: {
+        submit() {
+            if (this.$refs.form.validate()) {
+                console.log(this.title, this.content)
+            }
+        }
+    }
 }
 </script>
